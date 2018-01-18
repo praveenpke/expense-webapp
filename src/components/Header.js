4a8e2cbcd512fda671a5d60dabff0a067814698c
praveen emani <praveenemani@praveens-MacBook-Pro.local>
2018-01-17 23:58:35 -0600

    setup express

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () =>(
         <div>
             <NavLink exact to="/" activeClassName="is-active">Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-            <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+            
     
         </div>
     </header>
