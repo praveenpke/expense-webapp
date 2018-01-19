6904b3d7f3d37fe73ab3b06dd8bdfdf0f06946fe
praveen emani <praveenemani@praveens-MacBook-Pro.local>
2018-01-19 06:07:08 -0600

    finalized without styles

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,13 @@ import * as firebase from "firebase";
 
 
 var config = {
-    apiKey: "AIzaSyB0K0K0Ayt2D41AfsdnQ7kz_NfJ2mUfGrs",
-    authDomain: "expensify-f8d7c.firebaseapp.com",
-    databaseURL: "https://expensify-f8d7c.firebaseio.com",
-    projectId: "expensify-f8d7c",
-    storageBucket: "expensify-f8d7c.appspot.com",
-    messagingSenderId: "766336871557"
+   
+ apiKey: process.env.FIREBASE_API_KEY,
+ authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+ databaseURL: process.env.FIREBASE_DATABASE_URL,
+ projectId: process.env.FIREBASE_PROJECT_ID,
+ storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+ messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
